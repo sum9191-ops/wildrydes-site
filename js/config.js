08f2415ba92a0d37fec719f7f1fc2e20ef5ee629
sum9191-ops <sum9191@gmail.com>
2026-04-14 17:22:05 +0900

    invorkURL2

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://gc1fyxy0yh.execute-api.ap-northeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://gc1fyxy0yh.execute-api.ap-northeast-2.amazonaws.com/prod/ride' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
